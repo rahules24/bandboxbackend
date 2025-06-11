79456f7c6e584c32db2ade2519b85ab274adac2d
Rahul <rahul.es2024@gmail.com>
2025-06-11 14:08:25 +0530

    Use DATABASE_URL from environment

--- bbdBackend/settings.py
+++ bbdBackend/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = "bbdBackend.wsgi.application"
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
